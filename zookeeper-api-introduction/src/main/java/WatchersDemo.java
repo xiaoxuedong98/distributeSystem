70661685a9bab08f5c17e716221ee2f33aee0fe5
sarahdong <dong.xiaoxu@northeastern.edu>
2023-07-08 23:58:12 -0700

    add watchers

---
+++ zookeeper-api-introduction/src/main/java/WatchersDemo.java
@@ -0,0 +1,93 @@
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Watchers, Triggers and Introduction to Failure Detection
+ * https://www.udemy.com/course/distributed-systems-cloud-computing-with-java
+ */
+public class WatchersDemo implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private static final String TARGET_ZNODE = "/target_znode";
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws InterruptedException, IOException, KeeperException {
+        WatchersDemo watchersDemo = new WatchersDemo();
+        watchersDemo.connectToZookeeper();
+        watchersDemo.watchTargetZnode();
+        watchersDemo.run();
+        watchersDemo.close();
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+    public void watchTargetZnode() throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists(TARGET_ZNODE, this);
+        if (stat == null) {
+            return;
+        }
+
+        byte[] data = zooKeeper.getData(TARGET_ZNODE, this, stat);
+        List<String> children = zooKeeper.getChildren(TARGET_ZNODE, this);
+
+        System.out.println("Data : " + new String(data) + ", children : " + children);
+    }
+//    In the provided code, the WatchersDemo class implements the Watcher interface,
+//    which requires implementing the process method.
+//    The process method is responsible for handling different types of events triggered by ZooKeeper.
+//    These events include connection-related events, znode-related events
+//    (such as creation, deletion, data changes, and children changes),
+//    and others.
+    @Override
+    public void process(WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from Zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+                break;
+            case NodeDeleted:
+                System.out.println(TARGET_ZNODE + " was deleted");
+                break;
+            case NodeCreated:
+                System.out.println(TARGET_ZNODE + " was created");
+                break;
+            case NodeDataChanged:
+                System.out.println(TARGET_ZNODE + " data changed");
+                break;
+            case NodeChildrenChanged:
+                System.out.println(TARGET_ZNODE + " children changed");
+                break;
+        }
+
+        try {
+            watchTargetZnode();
+        } catch (KeeperException e) {
+        } catch (InterruptedException e) {
+        }
+    }
+}
